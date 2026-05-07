16db0096c39a560d6c2b9ce32de888975b3935c5
devgoagency-arch <devgoagency@gmail.com>
2026-05-07 09:34:23 -0500

    cambios de imagen y modal

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  image: {
+    domains: ['fiorefoodscanada.com']
+  }
 });
